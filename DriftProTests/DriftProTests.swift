3574e28fbe034dce67343aac81da67c63eb76b95
hazher <Hazhera@outlook.com>
2025-07-11 22:49:17 +0200

    Initial Commit

---
+++ DriftProTests/DriftProTests.swift
@@ -0,0 +1,16 @@
+//
+//  DriftProTests.swift
+//  DriftProTests
+//
+//  Created by Hazher  on 11/07/2025.
+//
+
+import Testing
+
+struct DriftProTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
