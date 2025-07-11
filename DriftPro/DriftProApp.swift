9ddebe647f8dbe5ca774a4679afa83b3c5469315
hazher <Hazhera@outlook.com>
2025-07-11 22:49:17 +0200

    Initial Commit

---
+++ DriftPro/DriftProApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriftProApp.swift
+//  DriftPro
+//
+//  Created by Hazher  on 11/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct DriftProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
